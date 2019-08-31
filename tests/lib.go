6b586085f991ea53ff37e23f5a4eb28a1f00ae7c
yeya24 <yb532204897@gmail.com>
2019-08-31 11:46:40 -0400

    add max min lib func

--- tests/lib.go
+++ tests/lib.go
@@ -12,3 +12,17 @@ type TreeNode struct {
 	Left  *TreeNode
 	Right *TreeNode
 }
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
