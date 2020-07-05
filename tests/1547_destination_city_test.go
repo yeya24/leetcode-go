c58195321af0bd1a39bb6e8b20ade714110cc652
yeya24 <yb532204897@gmail.com>
2020-07-05 14:30:32 -0400

    add 1547
    Signed-off-by: yeya24 <yb532204897@gmail.com>

---
+++ tests/1547_destination_city_test.go
@@ -0,0 +1,81 @@
+package tests
+
+import (
+    "testing"
+)
+
+/**
+ * [1547] Destination City
+ *
+ * You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the destination city, that is, the city without any path outgoing to another city.
+ * It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+ *  
+ * Example 1:
+ * 
+ * Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+ * Output: "Sao Paulo" 
+ * Explanation: Starting at "London" city you will reach "Sao Paulo" city which is the destination city. Your trip consist of: "London" -> "New York" -> "Lima" -> "Sao Paulo".
+ * 
+ * Example 2:
+ * 
+ * Input: paths = [["B","C"],["D","B"],["C","A"]]
+ * Output: "A"
+ * Explanation: All possible trips are: 
+ * "D" -> "B" -> "C" -> "A". 
+ * "B" -> "C" -> "A". 
+ * "C" -> "A". 
+ * "A". 
+ * Clearly the destination city is "A".
+ * 
+ * Example 3:
+ * 
+ * Input: paths = [["A","Z"]]
+ * Output: "Z"
+ * 
+ *  
+ * Constraints:
+ * 
+ * 	1 <= paths.length <= 100
+ * 	paths[i].length == 2
+ * 	1 <= cityAi.length, cityBi.length <= 10
+ * 	cityAi != cityBi
+ * 	All strings consist of lowercase and uppercase English letters and the space character.
+ * 
+ * 
+ */
+
+func TestDestinationCity(t *testing.T) {
+    var cases = []struct {
+        paths [][]string
+        output string
+    }{
+        {
+        	paths: [][]string{{"B", "C"}, {"D", "B"}, {"C", "A"}},
+            output: "A",
+        },
+    }
+    for _, c := range cases {
+        x := destCity(c.paths)
+        if x != c.output {
+            t.Fail()
+        }
+    }
+}
+
+// submission codes start here
+
+func destCity(paths [][]string) string {
+    cityMap := make(map[string]struct{})
+    for _, path := range paths {
+        cityMap[path[0]] = struct{}{}
+    }
+
+    for _, path := range paths {
+        if _, ok := cityMap[path[1]]; !ok {
+            return path[1]
+        }
+    }
+    return ""
+}
+
+// submission codes end
